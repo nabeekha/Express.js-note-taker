9d8c6e8c790c86f8110416e0ec85edad3286881f
nabeekha <nabeekha@umich.edu>
2023-04-05 21:41:38 -0400

    updated server files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const apiRoutes = ('./routes/apiroutes')
+const htmlRoutes = ('./routes/htmlroutes')
+
+const PORT = 3001
+
+const app = express()
+
+//creating middleware for parsing json
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+app.use(express.static('public'))
+
+app.listen(PORT, () =>
+    console.log(`app listening at http://localhost:${PORT}`)
+)
